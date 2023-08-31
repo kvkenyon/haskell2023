e56a66efa683c365460fb1d97e9e7bf14c9069eb
Kevin Kenyon <kvkenyon@gmail.com>
2023-08-30 21:25:32 -0500

    finish code golf rough draft

--- golf.hs
+++ golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List (sort,group, find)
+
 -- 0,1 ; 1,3, 2,5, 3,7
 -- abcd
 
@@ -11,3 +13,25 @@ every n xs = case drop (n-1) xs of
 
 skips:: [a] -> [[a]]
 skips xs = [every n xs | n <- [1..length xs]]
+
+localMaxima :: (Ord a) => [a] -> [a]
+localMaxima [] = []
+localMaxima [x] = []
+localMaxima [x,y] = []
+localMaxima (x:y:z:xs)
+        | y > x && y > z = y:localMaxima (y:z:xs)
+        | otherwise = localMaxima (y:z:xs)
+
+f :: [[Int]] -> [[Int]]
+f [] = []
+f xs = foldl (\acc x ->  if null x 
+        then acc else head x : acc) [] xs :  
+        f (filter (not.null) (map tail (filter (not . null) xs)))
+
+printLine :: [Int] -> String
+printLine xs = [c | i <- [0..9], c <- if find (==i) xs == Just i 
+    then "*" else " " ] ++ "\n"
+
+histogram :: [Int] -> String
+histogram xs = concatMap printLine (reverse . f . group $ sort xs)
+                 ++ "==========\n" ++ "0123456789\n"
