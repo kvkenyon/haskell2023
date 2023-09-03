12651e700338698013b574c16cc15f1cb4a6baf5
Kevin Kenyon <kvkenyon@gmail.com>
2023-09-03 10:50:21 -0500

    first iteration with path bug

--- quickestPath.hs
+++ quickestPath.hs
@@ -12,6 +12,8 @@ data RoadSystem
 
 type PathCost = Int
 
+type Path = [String]
+
 parseInput :: [String] -> [Int]
 parseInput = map read
 
@@ -19,10 +21,32 @@ buildRoadSystem :: [Int] -> RoadSystem
 buildRoadSystem [] = Destination
 buildRoadSystem (a : b : c : sections) = Section a b c $ buildRoadSystem sections
 
+computeSectionCost :: RoadSystem -> (PathCost, PathCost) -> Path -> Path -> (PathCost, PathCost, Path, Path)
+computeSectionCost Destination (a, b) pathA pathB = (a, b, pathA, pathB)
+computeSectionCost (Section roadA roadB crossRoad rest) (a, b) pathA pathB =
+  computeSectionCost
+    rest
+    ( min (a + roadA) (b + roadB + crossRoad),
+      min (b + roadB) (a + roadA + crossRoad)
+    )
+    ( pathA
+        ++ [pathToA]
+    )
+    ( pathB
+        ++ [pathToB]
+    )
+  where
+    pathToA = if a + roadA < b + roadB + crossRoad then "A" else "BC"
+    pathToB = if b + roadB < a + roadA + crossRoad then "B" else "AC"
+
 main :: IO () =
   do
     (fileName : _) <- getArgs
     contents <- readFile fileName
     let input = parseInput $ words contents
     let roads = buildRoadSystem input
-    print roads
+    let (costA, costB, pathA, pathB) = computeSectionCost roads (0, 0) [] []
+    let cost = min costA costB
+    let path = if costA < costB then pathA else pathB
+    print cost
+    print path
