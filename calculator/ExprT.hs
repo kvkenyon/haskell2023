a8772fc851ed870a320c4a27dda780711c41bd23
Kevin Kenyon <kvkenyon@gmail.com>
2023-09-06 23:08:10 -0500

    add type instances for expr type class

--- calculator/ExprT.hs
+++ calculator/ExprT.hs
@@ -1,6 +1,68 @@
 module ExprT where
 
-data ExprT = Lit Integer
-           | Add ExprT ExprT
-           | Mul ExprT ExprT
+import Data.Maybe
+import Parser (parseExp)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+data ExprT
+  = Lit Integer
+  | Add ExprT ExprT
+  | Mul ExprT ExprT
   deriving (Show, Eq)
+
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+instance Expr Integer where
+  lit x = x
+  add x y = x + y
+  mul x y = x * y
+
+instance Expr Bool where
+  lit x
+    | x <= 0 = False
+    | otherwise = True
+  add x y = x || y
+  mul x y = x && y
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+instance Expr Mod7 where
+  lit x = Mod7 $ x `mod` 7
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
+
+eval :: ExprT -> Integer
+eval (Lit x) = x
+eval (Add x y) = eval x + eval y
+eval (Mul x y) = eval x * eval y
+
+evalStr :: String -> Maybe Integer
+evalStr s = eval <$> parseExp Lit Add Mul s
+
+reify :: ExprT -> ExprT
+reify = id
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+
+testBool = testExp :: Maybe Bool
+
+testMM = testExp :: Maybe MinMax
+
+testSat = testExp :: Maybe Mod7
