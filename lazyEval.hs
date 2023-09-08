643a6f3ed301a35806a27ea480e1b9faaf5ce67f
Kevin Kenyon <kvkenyon@gmail.com>
2023-09-08 18:11:07 -0500

    Add fib and test

---
+++ lazyEval.hs
@@ -0,0 +1,14 @@
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib n - 1 + fib n - 2
+
+testFib :: [Bool]
+testFib =
+  [ fib 0 == 0,
+    fib 1 == 1,
+    fib 2 == 1,
+    fib 3 == 2,
+    fib 9 == 34,
+    fib 14 == 377
+  ]
