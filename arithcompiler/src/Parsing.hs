cdbc9b38474de28a68b6c28fba9981b9be018ab4
Kevin Kenyon <kvkenyon@gmail.com>
2023-09-27 17:33:29 -0500

    Fix problems in Parsing module

--- arithcompiler/src/Parsing.hs
+++ arithcompiler/src/Parsing.hs
@@ -1,6 +1,7 @@
 -- Version 3, 5 Oct 2016
 --   added type-specialized versions of (<*) and (*>) as well
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Parsing
   ( -- * Lexing
@@ -17,7 +18,7 @@ module Parsing
     getNaturalOrFloat,
     getSymbol,
     getWhiteSpace,
-    -- getParens,
+    getParens,
 
     -- * Parsing
     Parser,
@@ -38,13 +39,13 @@ module Parsing
 where
 
 import Control.Applicative qualified as A
-import Data.Functor.Identity
+import Data.Functor.Identity (Identity (runIdentity))
 import Text.Parsec qualified as P
 import Text.Parsec.Char
 import Text.Parsec.Combinator
 import Text.Parsec.Expr hiding (Operator)
 import Text.Parsec.Language (emptyDef)
-import Text.Parsec.Pos
+import Text.Parsec.Pos (initialPos)
 import Text.Parsec.Prim
   ( Consumed (..),
     Reply (..),
@@ -54,6 +55,22 @@ import Text.Parsec.Prim
   )
 import Text.Parsec.String (Parser, parseFromFile)
 import Text.Parsec.Token
+  ( GenLanguageDef (..),
+    GenTokenParser
+      ( float,
+        identifier,
+        integer,
+        natural,
+        naturalOrFloat,
+        parens,
+        reserved,
+        reservedOp,
+        symbol,
+        whiteSpace
+      ),
+    TokenParser,
+    makeTokenParser,
+  )
 import Prelude hiding ((*>), (<$), (<$>), (<*), (<*>))
 import Prelude qualified
 
@@ -61,25 +78,35 @@ import Prelude qualified
 -- Lexing
 ------------------------------------------------------------
 
+getIdentifier :: GenTokenParser s u m -> P.ParsecT s u m String
 getIdentifier = identifier
 
+getReserved :: GenTokenParser s u m -> String -> P.ParsecT s u m ()
 getReserved = reserved
 
+getReservedOp :: GenTokenParser s u m -> String -> P.ParsecT s u m ()
 getReservedOp = reservedOp
 
+getNatural :: GenTokenParser s u m -> P.ParsecT s u m Integer
 getNatural = natural
 
+getInteger :: GenTokenParser s u m -> P.ParsecT s u m Integer
 getInteger = integer
 
+getFloat :: GenTokenParser s u m -> P.ParsecT s u m Double
 getFloat = float
 
+getNaturalOrFloat :: GenTokenParser s u m -> P.ParsecT s u m (Either Integer Double)
 getNaturalOrFloat = naturalOrFloat
 
+getSymbol :: GenTokenParser s u m -> String -> P.ParsecT s u m String
 getSymbol = symbol
 
+getWhiteSpace :: GenTokenParser s u m -> P.ParsecT s u m ()
 getWhiteSpace = whiteSpace
 
--- getParens = parens
+getParens :: GenTokenParser s u m -> forall a. P.ParsecT s u m a -> P.ParsecT s u m a
+getParens = parens
 
 -- For more, see http://hackage.haskell.org/package/parsec-3.1.11/docs/Text-Parsec-Token.html
 
