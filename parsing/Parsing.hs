4647b318ebb4bbd8b5c3b6b428aa822edda99f1f
Kevin Kenyon <kvkenyon@gmail.com>
2023-10-02 17:57:40 -0500

    Finish module 6
    ghci> (tokenize)  "3-(4-5)"
    [TLit 3,TMinus,LParen,TLit 4,TMinus,TLit 5,RParen]
    ghci> (shunt.tokenize)  "3-(4-5)"
    [TLit 3,TLit 4,TLit 5,TMinus,TMinus]
    ghci> eval  "3-(4-5)"
    4
    ghci> eval "2+3-(4-5)"
    6
    ghci> eval "(2+3-(4-5))*6"
    36
    
    Can now parse and interp arith infix expr using shunting and postfix parsing to AST

--- parsing/Parsing.hs
+++ parsing/Parsing.hs
@@ -39,6 +39,8 @@ shunt' :: [Token] -> [Token] -> [Token]
 shunt' [] op = op
 shunt' (l@(TLit _) : toks) op = l : shunt' toks op
 shunt' (tok : toks) [] = shunt' toks [tok]
+shunt' (LParen : toks) (op : ops) = shunt' toks (LParen : op : ops)
+shunt' (RParen : toks) (op : ops) = if op == LParen then shunt' toks ops else op : shunt' (RParen : toks) ops
 shunt' (tok : toks) (op : ops) =
   if precedence tok > precedence op
     then shunt' toks (tok : op : ops)
@@ -46,3 +48,41 @@ shunt' (tok : toks) (op : ops) =
 
 shunt :: [Token] -> [Token]
 shunt input = shunt' input []
+
+data Op where
+  Plus :: Op
+  Minus :: Op
+  Times :: Op
+  deriving (Show, Eq)
+
+data Arith where
+  Lit :: Integer -> Arith
+  Bin :: Op -> Arith -> Arith -> Arith
+  deriving (Show)
+
+interp :: Arith -> Integer
+interp (Lit n) = n
+interp (Bin op a1 a2) = interpOp op (interp a1) (interp a2)
+
+interpOp :: Op -> Integer -> Integer -> Integer
+interpOp Plus = (+)
+interpOp Minus = (-)
+interpOp Times = (*)
+
+getOp :: Token -> Op
+getOp TMinus = Minus
+getOp TPlus = Plus
+getOp TTimes = Times
+
+parsePostfix' :: [Token] -> [Arith] -> Arith
+parsePostfix' [] (arith : rest) = arith
+parsePostfix' ((TLit x) : toks) stk = parsePostfix' toks (Lit x : stk)
+parsePostfix' (LParen : toks) stk = parsePostfix' toks stk
+parsePostfix' (RParen : toks) stk = parsePostfix' toks stk
+parsePostfix' (tok : toks) (y : x : stk) = parsePostfix' toks (Bin (getOp tok) x y : stk)
+
+parsePostfix :: [Token] -> Arith
+parsePostfix toks = parsePostfix' toks []
+
+eval :: String -> Integer
+eval = interp . parsePostfix . shunt . tokenize
