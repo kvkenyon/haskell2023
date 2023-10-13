b14d6b9a9b8aeaf681593f11c67b5f2f0a97c760
Kevin Kenyon <kvkenyon@gmail.com>
2023-10-13 18:45:13 -0500

    Add lexer and parser for quilt lang
    (base) ➜  quilt git:(main) ✗ stack ghci
    Using main module:
    1.  Package quilt, component quilt:exe:quilt, with main-is file: /Users/kevin/dev/haskell/quilt/QuiltREPL.hs.
    
    Configuring GHCi with the following packages: quilt.
    GHCi, version 9.4.7: https://www.haskell.org/ghc/  :? for help
    [1 of 4] Compiling Parsing2         ( Parsing2.hs, interpreted )
    [2 of 4] Compiling Quilt            ( Quilt.hs, interpreted )
    [3 of 4] Compiling Main             ( /Users/kevin/dev/haskell/quilt/QuiltREPL.hs, interpreted )
    Ok, three modules loaded.
    Loaded GHCi configuration from /Users/kevin/.cache/stack/ghci-script/d1007057/ghci-script
    ghci> parseSome qexpr "quilt 0.4 yellow red (quilt green orange blue purple)"
    Right (Quilt (LitNum (D 0.4)) (LitColor Yellow) (LitColor Red) (Quilt (LitColor Green) (LitColor Orange) (LitColor Blue) (LitColor Violet)),"")
    ghci> parseSome qexpr "3+True"
    Right (Arith Plus (LitNum (I 3)) (LitBool T),"")
    ghci> parseSome qexpr "quilt 0.4 yellow red (quilt green orange blue purple)"
    Right (Quilt (LitNum (D 0.4)) (LitColor Yellow) (LitColor Red) (Quilt (LitColor Green) (LitColor Orange) (LitColor Blue) (LitColor Violet)),"")
    ghci> parseSome qexpr "[(x+1)/2, (y+1)/2, 0.5]"
    Right (LitColorTrip (Arith Divide (Arith Plus (LitCoord X) (LitNum (I 1))) (LitNum (I 2))) (Arith Divide (Arith Plus (LitCoord Y) (LitNum (I 1))) (LitNum (I 2))) (LitNum (D 0.5)),"")
    ghci> parseSome qexpr "(x+1)/2*red + (1-(x+1)/2)*blue"
    Right (Arith Plus (Arith Times (Arith Divide (Arith Plus (LitCoord X) (LitNum (I 1))) (LitNum (I 2))) (LitColor Red)) (Arith Times (Arith Minus (LitNum (I 1)) (Arith Divide (Arith Plus (LitCoord X) (LitNum (I 1))) (LitNum (I 2)))) (LitColor Blue)),"")
    ghci> parseSome qexpr "[0.1, 2, 1]"
    Right (LitColorTrip (LitNum (D 0.1)) (LitNum (I 2)) (LitNum (I 1)),"")
    ghci> parseSome qexpr "if x then y else x"
    Right (If (LitCoord X) (LitCoord Y) (LitCoord X),"")
    ghci>

--- quilt/Quilt.hs
+++ quilt/Quilt.hs
@@ -4,6 +4,13 @@
 
 module Quilt where
 
+import qualified Data.Functor.Identity as I
+import Parsing2
+import Text.Parsec (ParseError)
+import Text.Parsec.Expr
+import qualified Text.Parsec.Prim as P
+import Text.Parsec.Token (reservedOp, symbol)
+
 -- | Color Literals represent ROYGBIV values
 --   They will be interpreted by triples of floating point numbers (Color)
 --   Each will be a constant
@@ -25,8 +32,8 @@ data Number where
 
 -- | Coordinate values have numbers for each dim
 data Coord where
-  X :: Number -> Coord
-  Y :: Number -> Coord
+  X :: Coord
+  Y :: Coord
   deriving (Show)
 
 data Boolean where
@@ -69,13 +76,181 @@ data QExpr where
   LitNum :: Number -> QExpr
   LitCoord :: Coord -> QExpr
   LitBool :: Boolean -> QExpr
-  LitColorTrip :: Color -> QExpr
+  LitColorTrip :: QExpr -> QExpr -> QExpr -> QExpr
   If :: QExpr -> QExpr -> QExpr -> QExpr
   Unary :: UOp -> QExpr -> QExpr
   Arith :: ArithOp -> QExpr -> QExpr -> QExpr
   Comp :: CompOp -> QExpr -> QExpr -> QExpr
+  Bool :: BoolOp -> QExpr -> QExpr -> QExpr
   Quilt :: QExpr -> QExpr -> QExpr -> QExpr -> QExpr
   deriving (Show)
+
+lexer :: TokenParser u
+lexer =
+  makeTokenParser
+    emptyDef
+      { opStart = oneOf "-+/*=<>&|!",
+        opLetter = oneOf "-+/*=<>&|!]",
+        reservedOpNames = ["-", "+", "/", "*", "==", "<", ">", "<=", ">=", "&&", "||", "!"],
+        reservedNames =
+          [ "True",
+            "False",
+            "if",
+            "then",
+            "else",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "indigo",
+            "purple",
+            "violet",
+            "quilt",
+            "x",
+            "y"
+          ]
+      }
+
+parens :: Parser a -> Parser a
+parens = getParens lexer
+
+number :: Parser (Either Integer Double)
+number = getNaturalOrFloat lexer
+
+operator :: String -> Parser ()
+operator = getReservedOp lexer
+
+reserved :: String -> Parser ()
+reserved = getReserved lexer
+
+whiteSpace :: Parser ()
+whiteSpace = getWhiteSpace lexer
+
+binary :: String -> (a -> a -> a) -> Assoc -> Operator String () I.Identity a
+binary name fun = Infix (do operator name; return fun)
+
+prefix :: String -> (a -> a) -> Operator String () I.Identity a
+prefix name fun = Prefix (do operator name; return fun)
+
+-- Parse Color Literals
+getColor :: String -> ColorLit
+getColor "red" = Red
+getColor "orange" = Orange
+getColor "yellow" = Yellow
+getColor "green" = Green
+getColor "blue" = Blue
+getColor "indigo" = Indigo
+getColor "violet" = Violet
+getColor "purple" = Violet
+getColor _ = error "Invalid color"
+
+color :: String -> Parser QExpr
+color c = reserved c >> return (LitColor (getColor c))
+
+parseColor :: Parser QExpr
+parseColor =
+  color "red"
+    <|> color "orange"
+    <|> color "yellow"
+    <|> color "green"
+    <|> color "blue"
+    <|> color "inidigo"
+    <|> color "purple"
+    <|> color "violet"
+
+-- Parse Numbers
+parseNumber :: Parser QExpr
+parseNumber = do
+  x <- number
+  case x of
+    Left i -> return $ LitNum $ I i
+    Right d -> return $ LitNum $ D d
+
+-- Parse coordinate
+x :: Parser QExpr
+x = reserved "x" >> return (LitCoord X)
+
+y :: Parser QExpr
+y = reserved "y" >> return (LitCoord Y)
+
+parseCoord :: Parser QExpr
+parseCoord = x <|> y
+
+-- Parse Boolean
+parseBool :: Parser QExpr
+parseBool = (reserved "True" >> return (LitBool T)) <|> (reserved "False" >> return (LitBool F))
+
+-- Parse Color Triple
+parseColorTriple :: Parser QExpr
+parseColorTriple = do
+  symbol lexer "["
+  a <- qexpr
+  whiteSpace
+  symbol lexer ","
+  b <- qexpr
+  whiteSpace
+  symbol lexer ","
+  c <- qexpr
+  whiteSpace
+  symbol lexer "]"
+  return $ LitColorTrip a b c
+
+parseIf :: Parser QExpr
+parseIf = do
+  reserved "if"
+  whiteSpace
+  test <- qexpr
+  whiteSpace
+  reserved "then"
+  whiteSpace
+  a <- qexpr
+  whiteSpace
+  reserved "else"
+  whiteSpace
+  b <- qexpr
+  whiteSpace
+  return $ If test a b
+
+parseQuilt :: Parser QExpr
+parseQuilt = do
+  reserved "quilt"
+  whiteSpace
+  a <- qexpr
+  whiteSpace
+  b <- qexpr
+  whiteSpace
+  c <- qexpr
+  whiteSpace
+  d <- qexpr
+  whiteSpace
+  return $ Quilt a b c d
+
+parseQExprAtom :: Parser QExpr
+parseQExprAtom = parseColor <|> parseNumber <|> parseBool <|> parseCoord
+
+term :: P.ParsecT String () I.Identity QExpr
+term = parens qexpr <|> parseQExprAtom <|> parseColorTriple <|> parseIf <|> parseQuilt
+
+table :: [[Operator String () I.Identity QExpr]]
+table =
+  [ [prefix "-" (Unary Neg), prefix "!" (Unary Not)],
+    [binary "*" (Arith Times) AssocLeft, binary "/" (Arith Divide) AssocLeft],
+    [binary "+" (Arith Plus) AssocLeft, binary "-" (Arith Minus) AssocLeft],
+    [ binary "==" (Comp Equal) AssocNone,
+      binary "<" (Comp Less) AssocNone,
+      binary "<=" (Comp LessEq) AssocNone,
+      binary ">=" (Comp GreaterEq) AssocNone,
+      binary ">" (Comp Greater) AssocNone
+    ],
+    [binary "&&" (Bool And) AssocLeft, binary "||" (Bool Or) AssocLeft]
+  ]
+
+qexpr :: P.ParsecT String () I.Identity QExpr
+qexpr = buildExpressionParser table term
+
+parseQExpr :: String -> Either ParseError QExpr
+parseQExpr = parse $ getWhiteSpace lexer *> qexpr <* eof
 
 -- | A quilt function produces a Color for any given location.  The
 --   parameters are x and y coordinates in the range [-1,1].
